d68ca2f0b681c7752da04f32e267fde7f67d46c0
Seemann <x87@users.noreply.github.com>
2026-05-29 13:41:23 -0400

    Delete copy.sh

--- copy.sh
+++
@@ -1,3 +0,0 @@
-cp -r dist/* "C:\work\GTA_GAMES\gta3\CLEO\III[mem]\\"
-mv "C:\work\GTA_GAMES\gta3\CLEO\III[mem]\main.ts" "C:\work\GTA_GAMES\gta3\CLEO\III[mem]\index.ts"
-mv "C:\work\GTA_GAMES\gta3\CLEO\III[mem]\main_d.ts" "C:\work\GTA_GAMES\gta3\CLEO\III[mem]\main_d.mts"
